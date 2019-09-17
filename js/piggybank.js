acb76735a08e8104a52a8ccf8c70574c200a8a7e
Adesina Opeoluwa Martins <opehardesina@gmail.com>
2019-09-17 15:48:54 +0100

    updating

--- js/piggybank.js
+++ js/piggybank.js
@@ -8,44 +8,83 @@ $(document).ready(function(){
         let lastname  = $('#lastname').val();
         let phone     = $('#phone').val();
         let email     = $('#email').val();
-        let password  = $('#password').val()
+        let password  = $('#pwd').val()
         //check if input field is empty
-        let error;
+        var error;
         if (firstname === '' || lastname==='' || phone ==='' || email ==='' || password==='') {
             error = true;
         }
 
-       //check if input information already exist      
+        
+      
+       //check if input information already exist     
+     
+       $.ajax({
+                type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
+                url         : 'http://localhost:3000/profiles', // the url where we want to POST
+                // data        : formData, // our data object
+                dataType    : 'json', // what type of data do we expect back from the server
+                // async: false,
+                success: function(res){
+                    // console.log(res);
+                     
+                    let errorArr =[];
+                    console.log(res.length);
+                    let count = 0;
+                    res.forEach(element => {
+                       if (element.email === email) {
+                        errorArr.push(true);
+                       } 
+
+                       
+    
+                       
+                        
+                      
+                    });
+                   console.log(errorArr);
+
+                    if (!errorArr.includes(true)) {
+                        var formData = {
+                            'firstname'          : firstname,
+                            'lastname'           : lastname,
+                            'phone'              : phone,
+                            'email'              : email,
+                            'password'           : password,
+                        };
+                
+                        // process the form
+                        $.ajax({
+                            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+                            url         : 'http://localhost:3000/profiles', // the url where we want to POST
+                            data        : formData, // our data object
+                            dataType    : 'json', // what type of data do we expect back from the server
+                            encode          : true
+                        })
+                            //using the done promise callback
+                            .done(function(data) {
+                                 
+                                // log data to the console so we can see
+                                console.log(data); 
+                
+                                // here we will handle errors and validation messages
+                            });
+                    }else{
+                        console.log('error');
+                    }
+    
+                    
+                }
+                           
+            })
+
+
+           
+       
+      
                        
 
-        if (!error) {
-            var formData = {
-                'firstname'          : firstname,
-                'lastname'           : lastname,
-                'phone'              : phone,
-                'email'              : email,
-                'password'           : password,
-            };
-    
-            // process the form
-            $.ajax({
-                type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-                url         : 'http://localhost:3000/profiles', // the url where we want to POST
-                data        : formData, // our data object
-                dataType    : 'json', // what type of data do we expect back from the server
-                encode          : true
-            })
-                //using the done promise callback
-                .done(function(data) {
-                     
-                    // log data to the console so we can see
-                    console.log(data); 
-    
-                    // here we will handle errors and validation messages
-                });
-        }else{
-            console.log('error');
-        }
+        
         
 
        
@@ -54,41 +93,6 @@ $(document).ready(function(){
     });
 
     
-    function formValidation(formInput,endPoint){
-
-        if (!formInput.isArray) {
-           return 'first arguement should be an array'; 
-        }
-        
-        $.ajax({
-            type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
-            url         : 'http://localhost:3000/profiles', // the url where we want to POST
-            // data        : formData, // our data object
-            dataType    : 'json', // what type of data do we expect back from the server
-            success: function(res){
-                // console.log(res);
-                console.log(res.length);
-                let count = 0;
-                res.forEach(element => {
-                console.log(element); 
-
-                if (element.email === email) {
-                    console.log('user email already exist');
-                }
-
-                count++;
-                    
-                
-                /*
-                    create a default pagination that breaks out of the loop after a certain
-                    count has been reached (10-20)
-
-                */
-                    console.log(count);
-                });
-            }
-                    
-        })
-    }
+    
 
 })
