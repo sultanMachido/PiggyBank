8fc33fe47ae2dd01d5de6edcbf667783f2348078
Adesina Opeoluwa Martins <opehardesina@gmail.com>
2019-09-20 06:50:32 +0100

    updating

--- js/register.js
+++ js/register.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
         var error;
         if (firstname === '' || lastname==='' || phone ==='' || email ==='' || password==='') {
             error = true;
+            
         }
 
         
